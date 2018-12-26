76a8149ed10c8445ed33572062f15a26b0e86096
sarthak7295 <sarthak.rawat@yahoo.com>
2018-12-26 23:16:54 +0530

    skeleton creator and line merging utility started

--- line_merging_utility.py
+++ line_merging_utility.py
@@ -6,29 +6,6 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pytesseract
 
-img = cv2.imread('test1.png', 0)
+def merge_lines(lines):
 
-cv2.imshow("skel", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-size = np.size(img)
-skel = np.zeros(img.shape, np.uint8)
-
-ret, img = cv2.threshold(img, 127, 255, 0)
-element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
-done = False
-
-while (not done):
-    eroded = cv2.erode(img, element)
-    temp = cv2.dilate(eroded, element)
-    temp = cv2.subtract(img, temp)
-    skel = cv2.bitwise_or(skel, temp)
-    img = eroded.copy()
-
-    zeros = size - cv2.countNonZero(img)
-    if zeros == size:
-        done = True
-
-cv2.imshow("skel", skel)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    return 0
